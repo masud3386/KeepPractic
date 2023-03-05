b41dff26013c2dc1d467bbf4d06e2036a9f8056b
masud3386 <masud3386@yahoo.com>
2023-03-04 22:12:44 -0500

    my first commit

---
+++ src/mustDoIt/ForLoop.java
@@ -0,0 +1,15 @@
+package mustDoIt;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i=0;i<=15;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
